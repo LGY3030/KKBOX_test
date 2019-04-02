9fe54413a4d52ba1f86863cdcd3ac06ee110211e
GuanYu Lin <lgy3030@gmail.com>
2019-04-02 21:44:15 +0800

    Update

--- KKBOX_Test_Train.py
+++ KKBOX_Test_Train.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[17]:
+# In[1]:
 
 
 from keras.utils import np_utils
@@ -13,7 +13,7 @@ from keras.optimizers import Adam
 import matplotlib.pyplot as plt
 
 
-# In[18]:
+# In[2]:
 
 
 train=pd.read_csv("data_processed/train.csv")
@@ -24,7 +24,7 @@ target=target.fillna(0)
 test=test.fillna(0)
 
 
-# In[19]:
+# In[3]:
 
 
 train=train.drop(["Unnamed: 0"], axis=1)
@@ -32,7 +32,7 @@ test=test.drop(["Unnamed: 0"], axis=1)
 target=target.drop(["Unnamed: 0"], axis=1)
 
 
-# In[20]:
+# In[4]:
 
 
 train=train.convert_objects(convert_numeric=True)
@@ -40,14 +40,14 @@ test=test.convert_objects(convert_numeric=True)
 target=target.convert_objects(convert_numeric=True)
 
 
-# In[21]:
+# In[5]:
 
 
 train=(train-train.mean())/train.std()
 test=(test-test.mean())/test.std()
 
 
-# In[22]:
+# In[6]:
 
 
 train=np.array(train)
@@ -56,29 +56,39 @@ target=np.array(target)
 target=target.reshape(-1)
 
 
-# In[23]:
+# In[7]:
 
 
 model=Sequential()
-model.add(Dense(units=100,input_dim=23,kernel_initializer='uniform',activation='relu'))
-model.add(Dropout(0.1))
-model.add(Dense(units=50,kernel_initializer='uniform',activation='relu'))
+model.add(Dense(units=1000,input_dim=23,kernel_initializer='uniform',activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(units=100,kernel_initializer='uniform',activation='relu'))
 model.add(Dense(units=1,kernel_initializer='uniform',activation='sigmoid'))
 model.summary()
-model.compile(loss='binary_crossentropy',optimizer=Adam(lr=0.0005),metrics=['accuracy'])
+model.compile(loss='binary_crossentropy',optimizer=Adam(lr=0.001),metrics=['accuracy'])
 train_history=model.fit(x=train,y=target,validation_split=0.1,epochs=30,batch_size=30,verbose=1)
-def show_train_history(train_history,train,validation):
+def show_train_history(train_history,train,validation,name):
     plt.plot(train_history.history[train])
     plt.plot(train_history.history[validation])
     plt.title('Train History')
     plt.ylabel(train)
     plt.xlabel('Epoch')
     plt.legend(['train','validation'],loc='upper left')
-    plt.savefig('result.jpg')
+    plt.savefig(name+'.jpg')
     plt.clf()
-show_train_history(train_history,'acc','val_acc')
-show_train_history(train_history,'loss','val_loss')
+show_train_history(train_history,'acc','val_acc','acc_image')
+show_train_history(train_history,'loss','val_loss','loss_image')
 result=pd.DataFrame({'acc':train_history.history['acc'],'val_acc':train_history.history['val_acc'],'loss':train_history.history['loss'],'val_loss':train_history.history['val_loss']})
 result.to_csv("target.csv", encoding='utf_8_sig')
 model.save('model.h5')
+
+
+# In[19]:
+
+
+get_predict=model.predict_classes(test)
+get_predict=pd.DataFrame(get_predict)
+get_predict=get_predict.reset_index()
+get_predict.columns=['id','target']
+get_predict.to_csv("submission.csv", encoding='utf_8_sig')
 
